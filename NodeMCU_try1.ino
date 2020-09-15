fd00c9956ace6ae79a1b32eb958c8331a8546aa4
Positron Robotics <positronrobotics@gmail.com>
2020-09-15 20:29:54 +0530

    Branching out

--- NodeMCU_try1.ino
+++ NodeMCU_try1.ino
@@ -32,6 +32,8 @@
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
 
+#include <Wire.h>
+
 // You should get Auth Token in the Blynk App.
 // Go to the Project Settings (nut icon).
 char auth[] = "uKrfRqfljEc1KtnNXqikouIxrVut-97-";
@@ -46,10 +48,36 @@ BLYNK_WRITE(V1) {
   int y = param[1].asInt();
 
   // Do something with x and y
-  Serial.print("X is = ");
+  Serial.print("X = ");
   Serial.print(x);
-  Serial.print("; Y is = ");
+  Serial.print("; Y = ");
   Serial.println(y);
+
+  Wire.beginTransmission(8); /* begin with device address 8 */
+
+  Wire.write("X = ");
+  Wire.write(x);
+  Wire.write("; Y = ");
+  Wire.write(y);
+  
+  Wire.endTransmission();  
+}
+
+BLYNK_WRITE(V2) {
+  int m1 = param.asInt();
+
+  Wire.beginTransmission(8); /* begin with device address 8 */
+
+  if(m1==1)
+  {
+    Wire.write("m1=1");
+  }
+  else
+  {
+    Wire.write("m1=0");
+  }
+  
+  Wire.endTransmission();  
 }
 
 void setup()
@@ -61,9 +89,17 @@ void setup()
   // You can also specify server:
   //Blynk.begin(auth, ssid, pass, "blynk-cloud.com", 80);
   //Blynk.begin(auth, ssid, pass, IPAddress(192,168,1,100), 8080);
+
+  Wire.begin(D1, D2);
+
+  Wire.beginTransmission(8); /* begin with device address 8 */
+
+  Wire.write("NodeMCU starts");
+  
+  Wire.endTransmission();    
 }
 
 void loop()
 {
-  Blynk.run();
+  Blynk.run();   
 }
