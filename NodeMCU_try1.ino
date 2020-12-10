e6c53d8912a2b8471f1c725b4eea8cdaa3a751b4
Positron Robotics <positronrobotics@gmail.com>
2020-12-10 13:15:44 +0530

    going on

--- NodeMCU_try1.ino
+++ NodeMCU_try1.ino
@@ -20,10 +20,10 @@ BLYNK_WRITE(V1) {
   int y = param[1].asInt();
 
   // Do something with x and y
-  Serial.print("X = ");
-  Serial.print(x);
-  Serial.print("; Y = ");
-  Serial.println(y);
+  //Serial.print("X = ");
+  //Serial.print(x);
+  //Serial.print("; Y = ");
+  //Serial.println(y);
 
   Wire.beginTransmission(8); /* begin with device address 8 */
 
@@ -40,10 +40,12 @@ BLYNK_WRITE(V2) {
   if(m1==1)
   {
     Wire.write("m1=1");
+    //Serial.println("m1=1");
   }
   else
   {
     Wire.write("m1=0");
+    //Serial.println("m1=0");
   }
   
   Wire.endTransmission();  
@@ -55,6 +57,9 @@ BLYNK_WRITE(V3) {
   Wire.beginTransmission(8); /* begin with device address 8 */
 
   SendInt(valV3);
+
+  //Serial.print("valV3=");
+  //Serial.println(valV3);
   
   Wire.endTransmission();  
 }
@@ -63,6 +68,8 @@ void setup()
 {
   // Debug console
   Serial.begin(9600);
+
+  Serial.print("PT100");
 
   Blynk.begin(auth, ssid, pass);
   // You can also specify server:
@@ -81,7 +88,7 @@ void setup()
 void SendInt(int value)
 {
     char values[4];
-    sprintf(values,"%04d",value);
+    //sprintf(values,"%04d",value);
 
     Wire.write(values, 4);
 }
